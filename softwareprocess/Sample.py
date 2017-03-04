9884fb52e8a1d68bdb2e7f8a8c898569d00aeade
Rain Li <yrl0001@auburn.edu>
2017-03-04 17:09:00 -0600

    fixed format in sample.py

--- softwareprocess/Sample.py
+++ softwareprocess/Sample.py
@@ -1,52 +1,53 @@
 import math
-class Sample(object):
 
-# outward facing methods
+
+# noinspection PyPep8Naming
+class Sample(object):
+    # outward facing methods
     def __init__(self, n=None):
         functionName = "Sample.__init__: "
-        if(n == None):
+        if n is None:
             raise ValueError(functionName + "invalid n")
-        if(not(isinstance(n, int))):
+        if not (isinstance(n, int)):
             raise ValueError(functionName + "invalid n")
-        if((n < 2) or (n >= 30)):
+        if (n < 2) or (n >= 30):
             raise ValueError(functionName + "invalid n")
         self.n = n
-
+    
     def getN(self):
         return self.n
-
     
     def p(self, t=None, tails=1):
         functionName = "Sample.p: "
-        if(t == None):
+        if t is None:
             raise ValueError(functionName + "missing t")
-        if(not(isinstance(t, float))):
+        if not (isinstance(t, float)):
             raise ValueError(functionName + "invalid t")
-        if(t < 0.0):
+        if t < 0.0:
             raise ValueError(functionName + "invalid t")
         
-        if(not(isinstance(tails, int))):
+        if not (isinstance(tails, int)):
             raise ValueError(functionName + "invalid tails")
-        if((tails != 1) & (tails != 2)):
+        if (tails != 1) & (tails != 2):
             raise ValueError(functionName + "invalid tails")
         
         constant = self.calculateConstant(self.n)
-        integration = self.integrate(0.0,t, self.n, self.f)
-        if(tails == 1):
+        integration = self.integrate(0.0, t, self.n, self.f)
+        if tails == 1:
             result = constant * integration + 0.5
         else:
             result = constant * integration * 2
-            
-        if(result > 1.0):
+        
+        if result > 1.0:
             raise ValueError(functionName + "result > 1.0")
         
         return result
-        
-# internal methods
+    
+    # internal methods
     def gamma(self, x):
-        if(x == 1):
+        if x == 1:
             return 1
-        if(x == 0.5):
+        if x == 0.5:
             return math.sqrt(math.pi)
         return (x - 1) * self.gamma(x - 1)
     
@@ -74,11 +75,11 @@ class Sample(object):
             s *= 2
             w = (high_bound - low_bound) / s
             middle_terms = 0
-            for i in range(1, s-1):
-                #i is odd
+            for i in range(1, s - 1):
+                # i is odd
                 if i % 2 == 1:
-                    middle_terms = middle_terms + 4 * f(low_bound+i*w, n) + 2 * f(low_bound+(i+1)*w, n)
+                    middle_terms = middle_terms + 4 * f(low_bound + i * w, n) + 2 * f(low_bound + (i + 1) * w, n)
             
-            simpson_new = (w/3) * (f(low_bound, n) + middle_terms + 4 * f(high_bound - w, n) + f(high_bound, n))
+            simpson_new = (w / 3) * (f(low_bound, n) + middle_terms + 4 * f(high_bound - w, n) + f(high_bound, n))
         
         return simpson_new
