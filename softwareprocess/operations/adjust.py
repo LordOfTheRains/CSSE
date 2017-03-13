f7d6eb0b406834cf2e242217662618dead064048
Rain Li <yrl0001@auburn.edu>
2017-03-12 22:43:56 -0500

    assignment 4 initial commit including given source code and new skeleton code

---
+++ softwareprocess/operations/adjust.py
@@ -0,0 +1,24 @@
+from operation import Operation
+
+
+class Adjust(Operation):
+    
+    def __init__(self):
+        Operation.__init__(self)
+        pass
+    
+    def validate_parameter(self):
+        """
+        this method will validate each input parameter. the validation includes type check and range check
+        all appropriate errors shall be thrown within the implementation
+        :return: boolean indicating success/failure
+        """
+        return True
+    
+    def perform(self):
+        """
+        this method will perform the calculation for designated operation
+        and attach the result as a key value pair to the original input dictionary
+        :return: original dictionary + result key-value pair
+        """
+        return {}
---
+++ softwareprocess/operations/operation.py
@@ -0,0 +1,28 @@
+from abc import ABCMeta, abstractmethod
+
+
+class Operation:
+    __metaclass__ = ABCMeta
+
+    def __init__(self):
+        pass
+    
+    @abstractmethod
+    def validate_parameter(self):
+        """
+        this method will validate each input parameter. the validation includes type check and range check
+        all appropriate errors shall be thrown within the implementation
+        :return: boolean indicating success/failure
+        """
+        raise NotImplemented
+
+    @abstractmethod
+    def perform(self):
+        """
+        this method will perform the calculation for designated operation
+        and attach the result as a key value pair to the original input dictionary
+        :return: original dictionary + result key-value pair
+        """
+    raise NotImplemented
+
+
