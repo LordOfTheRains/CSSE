1379c5a6cb6e458480f4938876d45daef8abbc76
Rain Li <yrl0001@auburn.edu>
2017-03-12 23:20:28 -0500

    Red Light |

---
+++ test/operations/testAdjustClass.py
@@ -0,0 +1,27 @@
+from __future__ import absolute_import
+
+import unittest
+from softwareprocess.operations.adjust import Adjust
+
+
+class SampleTest(unittest.TestCase):
+    
+    def setUp(self):
+        pass
+    
+    def tearDown(self):
+        pass
+    
+    def test_validate_parameter_observation(self):
+        # observation: mandatory, unvalidated
+        # string: xdy.y
+        
+        # Happy path
+        # observation is in the form of xdy.y
+        # 0 <= x < 90
+        # 0.0 <= y < 60
+        adjust = adjust()
+        pass
+    
+    def test_validate_parameter_height(self):
+        pass
