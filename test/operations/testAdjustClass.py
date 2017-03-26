2a9c0ad4b9847036e61d76a673d1edc20b3eb249
Rain Li <yrl0001@auburn.edu>
2017-03-26 17:07:46 -0500

    Red Light |

--- test/operations/testAdjustClass.py
+++ test/operations/testAdjustClass.py
@@ -36,7 +36,9 @@ class AdjustClassTest(unittest.TestCase):
         # observation is in the form of xdy.y
         # 0 <= x < 90
         # 0.0 <= y < 60
-        self.assertRaises(TypeError, Adjust.validate_parameter())
+        self.assertRaises(TypeError, Adjust.validate_parameter({'observation': '15d04.9', 'height': '6.0',
+                                                                'pressure': '1010', 'horizon': 'artificial',
+                                                                'op': 'adjust', 'temperature': '72'}))
         pass
     
     def test_validate_parameter_height(self):
