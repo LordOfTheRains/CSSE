902d9808bb2c2944cfa8b88be70bea5bac9a34a2
Rain Li <yrl0001@auburn.edu>
2017-03-26 17:19:27 -0500

    Green Light |

--- test/operations/AdjustTest.py
+++ test/operations/AdjustTest.py
@@ -15,8 +15,13 @@ class AdjustTest(unittest.TestCase):
     def test_validate_parameter_dictionary_exist(self):
         # observation: mandatory, unvalidated
         # string: xdy.y
-        self.assertRaises(TypeError, Adjust.validate_parameter())
-        self.assertRaises(TypeError, Adjust.validate_parameter("asdad"))
+        expected_string = "No Valid Dictionary Provided"
+        with self.assertRaises(TypeError) as context:
+            Adjust.validate_parameter()
+        self.assertEquals(expected_string, context.exception.args[0][0:len(expected_string)])
+        with self.assertRaises(TypeError) as context:
+            Adjust.validate_parameter("asdad"
+        self.assertEquals(expected_string, context.exception.args[0][0:len(expected_string)])
         # Happy path
         # observation is in the form of xdy.y
         # 0 <= x < 90
@@ -35,8 +40,8 @@ class AdjustTest(unittest.TestCase):
         # 0 <= x < 90
         # 0.0 <= y < 60
         self.assertTrue(Adjust.validate_parameter({'observation': '15d04.9', 'height': '6.0',
-                                                                'pressure': '1010', 'horizon': 'artificial',
-                                                                'op': 'adjust', 'temperature': '72'}))
+                                                   'pressure': '1010', 'horizon': 'artificial',
+                                                   'op': 'adjust', 'temperature': '72'}))
         
         # Sad Path
         # missing key in the dictionary
