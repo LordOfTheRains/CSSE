728923a276b5412317f4199584744360a1396c7d
Rain Li <yrl0001@auburn.edu>
2017-03-26 22:35:55 -0500

    Green Light |

--- test/operations/AdjustTest.py
+++ test/operations/AdjustTest.py
@@ -29,8 +29,8 @@ class AdjustTest(unittest.TestCase):
         input_dict = {'observation': '45d15.2', 'height': '6',
                       'pressure': '1010', 'horizon': 'natural',
                       'op': 'adjust', 'temperature': '71'}
-        expected = {'altitude':'45d11.9', 'observation': '30d1.5',
-                    'height': '19.0', 'pressure': '1000',
+        expected = {'altitude': '45d11.9', 'observation': '30d1.5',
+                    'height': '6', 'pressure': '1000',
                     'horizon': 'artificial', 'op': 'adjust',
                     'temperature': '85'}
         
