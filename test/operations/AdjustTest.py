30681739737dff2d9f8f5ccf12b95d782ddd1c2f
Rain Li <yrl0001@auburn.edu>
2017-03-26 19:13:00 -0500

    Green Light |

--- test/operations/AdjustTest.py
+++ test/operations/AdjustTest.py
@@ -186,3 +186,22 @@ class AdjustTest(unittest.TestCase):
         self.assertTrue(Adjust.validate_parameter({'observation': '15d04.9', 'height': '6.0',
                                                    'pressure': '1010', 'horizon': 'natural',
                                                    'op': 'adjust', 'temperature': '72'}))
+        
+    def test_validate_parameter_no_altitude(self):
+        # altitude: forbidden, unvalidated
+        
+        # Sad Path
+        
+        # invalid horizon value
+        expected_string = "Input Dictionary Contains Forbidden Parameter: altitude"
+        validated = Adjust.validate_parameter({'observation': '15d04.9', 'height': '6.0',
+                                               'pressure': '1010', 'horizon': 'natural',
+                                               'op': 'adjust', 'temperature': '72', 'altitude': "asdsad"})
+        self.assertTrue((expected_string in validated))
+        # Happy path
+        
+        # key exist
+        self.assertTrue(Adjust.validate_parameter({'observation': '15d04.9', 'height': '6.0',
+                                                   'pressure': '1010', 'horizon': 'artificial',
+                                                   'op': 'adjust', 'temperature': '72'}))
+
