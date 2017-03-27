6b6684ea8abe0875e490cf039a3242a12da5b8ca
Rain Li <yrl0001@auburn.edu>
2017-03-26 22:32:02 -0500

    Green Light |

--- softwareprocess/operations/adjust.py
+++ softwareprocess/operations/adjust.py
@@ -144,6 +144,7 @@ class Adjust(Operation):
         else:
             dip = 0
         observed = self.observation_degree + self.observation_minute/60
+        observed_radian = observed * math.pi/180
         print (observed)
         refraction = ((-0.00452*self.pressure)/(273+(self.temperature-32)*5/9))/math.tan(observed)
         adjusted = observed + dip + refraction
