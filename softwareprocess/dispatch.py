6db906dd3b5d04674127dcf14399c9c607c6664d
Rain Li <yrl0001@auburn.edu>
2017-03-30 07:20:00 -0500

    Refactor |

--- softwareprocess/dispatch.py
+++ softwareprocess/dispatch.py
@@ -3,30 +3,30 @@ from operations.adjust import Adjust
 
 def dispatch(values=None):
 
-    #Validate parm
-    if(values == None):
+    # Validate parm
+    if values is None:
         return {'error': 'parameter is missing'}
-    if(not(isinstance(values,dict))):
+    if not(isinstance(values,dict)):
         return {'error': 'parameter is not a dictionary'}
-    if (not('op' in values)):
+    if not('op' in values):
         values['error'] = 'no op  is specified'
         return values
 
-    #Perform designated function
-    if(values['op'] == 'adjust'):
+    # Perform designated function
+    if values['op'] == 'adjust':
         validated = Adjust.validate_parameter(values)
         if validated is True:
             adj = Adjust(values)
             return adj.perform()
         else:
             values['error'] = validated
-        return values    #<-------------- replace this with your implementation
-    elif(values['op'] == 'predict'):
-        return values    #This calculation is stubbed out
-    elif(values['op'] == 'correct'):
-        return values    #This calculation is stubbed out
-    elif(values['op'] == 'locate'):
-        return values    #This calculation is stubbed out
+        return values    # <-------------- replace this with your implementation
+    elif values['op'] == 'predict':
+        return values    # This calculation is stubbed out
+    elif values['op'] == 'correct':
+        return values    # This calculation is stubbed out
+    elif values['op'] == 'locate':
+        return values    # This calculation is stubbed out
     else:
         values['error'] = 'op is not a legal operation'
         return values
