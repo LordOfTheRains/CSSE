cb4502e500670ac432a21f6a2facbe1ecaa0dd10
Rain Li <yrl0001@auburn.edu>
2017-03-30 20:19:56 -0500

    Green Light |

--- test/operations/DispatchTest.py
+++ test/operations/DispatchTest.py
@@ -6,7 +6,6 @@ from softwareprocess import dispatch as dispatcher
 
 
 class DispatchTest(unittest.TestCase):
-    
     def setUp(self):
         pass
     
@@ -25,7 +24,7 @@ class DispatchTest(unittest.TestCase):
                     'temperature': '85'}
         result = dispatcher.dispatch(input_dict)
         self.assertEqual(result, expected)
-            
+        
         input_dict = {'observation': '45d15.2', 'height': '6',
                       'pressure': '1010', 'horizon': 'natural',
                       'op': 'adjust', 'temperature': '71'}
@@ -36,35 +35,35 @@ class DispatchTest(unittest.TestCase):
         
         result = dispatcher.dispatch(input_dict)
         self.assertEqual(result, expected)
-            
-        input_dict = {'observation': '42d0.0',  'op': 'adjust'}
-        expected = {'altitude': '41d59.0', 'observation': '42d0.0',  'op': 'adjust'}
-        result = dispatcher.dispatch(input_dict)
-        self.assertEqual(result, expected)
-            
-        input_dict = {'observation': '42d0.0',  'op': 'adjust', 'extraKey': 'ignore'}
-        expected = {'altitude': '41d59.0', 'observation': '42d0.0',  'op': 'adjust', 'extraKey':'ignore'}
+        
+        input_dict = {'observation': '42d0.0', 'op': 'adjust'}
+        expected = {'altitude': '41d59.0', 'observation': '42d0.0', 'op': 'adjust'}
         result = dispatcher.dispatch(input_dict)
         self.assertEqual(result, expected)
         
+        input_dict = {'observation': '42d0.0', 'op': 'adjust', 'extraKey': 'ignore'}
+        expected = {'altitude': '41d59.0', 'observation': '42d0.0', 'op': 'adjust', 'extraKey': 'ignore'}
+        result = dispatcher.dispatch(input_dict)
+        self.assertEqual(result, expected)
+    
     def test_dispatch_invalid_adjust(self):
         input_dict = {'op': 'adjust'}
-
+        
         result = dispatcher.dispatch(input_dict)
         self.assertTrue("error" in result)
-            
+        
         input_dict = {'observation': '101d15.2',
                       'height': '6', 'pressure': '1010',
                       'horizon': 'natural', 'op': 'adjust',
                       'temperature': '71'}
-
+        
         result = dispatcher.dispatch(input_dict)
         self.assertTrue("error" in result)
         
         input_dict = {'observation': '45d15.2', 'height': 'a',
                       'pressure': '1010', 'horizon': 'natural',
                       'op': 'adjust', 'temperature': '71'}
-
+        
         result = dispatcher.dispatch(input_dict)
         self.assertTrue("error" in result)
         
@@ -72,17 +71,17 @@ class DispatchTest(unittest.TestCase):
                       'height': '6', 'horizon': '   ',
                       'pressure': '1010', 'op': 'adjust',
                       'temperature': '71'}
-
+        
         result = dispatcher.dispatch(input_dict)
         self.assertTrue("error" in result)
-
+    
     # Predict Test
     def test_dispatch_valid_predict(self):
-        # input_dict = {'name': 'BetelGeuse', 'date': '2016-01-17',
-        #               'time': '03:15:42', 'op': 'predict'}
-        # expected = {'op': 'predict', 'body': 'Betelgeuse',
-        #             'date': '2016-01-17', 'time': '03:15:42',
-        #             'long': '75d53.6', 'lat':'7d24.3'}
-        #
-        # result = dispatcher.dispatch(input_dict)
-        # self.assertEqual(result, expected)
+# input_dict = {'name': 'BetelGeuse', 'date': '2016-01-17',
+#               'time': '03:15:42', 'op': 'predict'}
+# expected = {'op': 'predict', 'body': 'Betelgeuse',
+#             'date': '2016-01-17', 'time': '03:15:42',
+#             'long': '75d53.6', 'lat':'7d24.3'}
+#
+# result = dispatcher.dispatch(input_dict)
+# self.assertEqual(result, expected)
