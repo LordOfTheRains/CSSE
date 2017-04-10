a3fb1803f55aab8ade4cb5958a042116316e8241
Rain Li <yrl0001@auburn.edu>
2017-04-09 22:03:16 -0500

    Green Light |

--- softwareprocess/utility/angle.py
+++ softwareprocess/utility/angle.py
@@ -31,9 +31,8 @@ class Angle:
         :param angle2: '''
         :return: result angle
         """
-        rounded1 = round(angle1.decimal, 6)
-        rounded2 = round(angle2.decimal, 6)
-        result = round((angle2.decimal + angle1.decimal), 6)
+        
+        result = angle1.decimal + angle2.decimal
         print("added result:")
         print(result)
         return Angle.from_decimal(result)
