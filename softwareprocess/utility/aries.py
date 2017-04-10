2b79690290b7d022289bae304117b33a7f8c5607
Rain Li <yrl0001@auburn.edu>
2017-04-09 21:15:43 -0500

    Green Light |

--- softwareprocess/utility/aries.py
+++ softwareprocess/utility/aries.py
@@ -73,7 +73,8 @@ class Aries:
         :return: hour angle
         """
         full_angle = Angle.from_string("360d00.0")
-        rotation = round(elapsed_seconds/86164.1000, 3)
+        elapsed_seconds = float(elapsed_seconds,3)
+        rotation = round(elapsed_seconds/86164.1000, 5)
         print("full_angle_:" + full_angle.str)
         print("rotation_:" + str(rotation))
         print("get_earth_rotation_:" + Angle.from_decimal(rotation).str)
