713e54ce5ebb3bfa140194fe51edc7c43f568ece
Rain Li <yrl0001@auburn.edu>
2017-04-09 21:42:00 -0500

    Green Light |

--- softwareprocess/operations/predict.py
+++ softwareprocess/operations/predict.py
@@ -99,15 +99,15 @@ class Predict(Operation):
         sec = int(sec)
         star_gha = Aries.get_greenwich_hour_angle(year, month, day, hour, minute, sec)
         star_sha = Angle.from_string(self.sidereal)
-        print ("star_gha.str")
-        print (star_sha.str)
+        
         longitude = Angle.add(star_gha, star_sha)
         full_angle = Angle.from_string("-360d0.0")
         if longitude.hour_degree > 360:
             longitude = Angle.add(longitude, full_angle)
         if longitude.hour_degree == 360 and longitude.minute_degree > 0:
             longitude = Angle.add(longitude, full_angle)
-            
+        print ("star_gha.str")
+        print (longitude.str)
         self.original['lat'] = lat.str
         self.original['long'] = longitude.str
         return self.original
