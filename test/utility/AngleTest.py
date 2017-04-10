0e45b7cb683a28310fcad7a4462eb7f170d7eec5
Rain Li <yrl0001@auburn.edu>
2017-04-09 20:21:07 -0500

    Green Light |

--- test/utility/AngleTest.py
+++ test/utility/AngleTest.py
@@ -93,15 +93,15 @@ class AriesTest(unittest.TestCase):
         angle = Angle.from_string("164d54.5")
         self.assertEqual(angle.hour_degree, 164)
         self.assertEqual(angle.minute_degree, 54.5)
-        self.assertAlmostEqual(angle.decimal, 0.45, places=3)
+        self.assertAlmostEqual(angle.decimal, 0.458, places=3)
         
         angle = Angle.from_string("64d49.7")
         self.assertEqual(angle.hour_degree, 64)
         self.assertEqual(angle.minute_degree, 49.7)
-        self.assertAlmostEqual(angle.decimal, 64.828, places=3)
+        self.assertAlmostEqual(angle.decimal, 0.180, places=3)
         
         angle = Angle.from_string("-64d49.7")
         self.assertEqual(angle.hour_degree, -64)
         self.assertEqual(angle.minute_degree, 49.7)
-        self.assertAlmostEqual(angle.decimal, -64.828, places=3)
+        self.assertAlmostEqual(angle.decimal, -0.180, places=3)
         pass
