0e309c84b60bd6612eb3662c1a904d56318f2140
Rain Li <yrl0001@auburn.edu>
2017-04-27 18:38:20 -0500

    Green Light |

--- test/operations/CorrectTest.py
+++ test/operations/CorrectTest.py
@@ -16,13 +16,52 @@ class AriesTest(unittest.TestCase):
         # tests following mandatory parameter presence
         # lat xdyy.y
         # long xdyy.y
-        # altititude xdyy.y
+        # altitude xdyy.y
         # assumedLat xdyy.y
         # assumedLong xdyy.y
         expected = "mandatory information is missing"
         
-        input_dict = {'op': 'correct', 'lat':"asdsad", 'long':"adsa",
+        # happy path
+        # all parameter present
+        input_dict = {'op': 'correct', 'lat': "asdsad", 'long': "adsa",
                       'assumedLat': 'unknown', 'assumedLong': '2016-01-17',
                       'altitude': '03:15:99'}
         result = Correct.validate_parameter(input_dict)
         self.assertTrue(expected not in result)
+        
+        # sad path
+        # missing lat
+        
+        input_dict = {'op': 'correct', 'lat1': "asdsad", 'long': "adsa",
+                      'assumedLat': 'unknown', 'assumedLong': '2016-01-17',
+                      'altitude': '03:15:99'}
+        result = Correct.validate_parameter(input_dict)
+        self.assertTrue(expected in result)
+
+        # missing long
+        input_dict = {'op': 'correct', 'lat': "asdsad", 'long1': "adsa",
+                      'assumedLat': 'unknown', 'assumedLong': '2016-01-17',
+                      'altitude': '03:15:99'}
+        result = Correct.validate_parameter(input_dict)
+        self.assertTrue(expected in result)
+        
+        # missing altitude
+        input_dict = {'op': 'correct', 'lat': "asdsad", 'long': "adsa",
+                      'assumedLat': 'unknown', 'assumedLong': '2016-01-17',
+                      'altitude1': '03:15:99'}
+        result = Correct.validate_parameter(input_dict)
+        self.assertTrue(expected in result)
+        
+        # missing assumedLat
+        input_dict = {'op': 'correct', 'lat': "asdsad", 'long': "adsa",
+                      'assumed1Lat': 'unknown', 'assumedLong': '2016-01-17',
+                      'altitude': '03:15:99'}
+        result = Correct.validate_parameter(input_dict)
+        self.assertTrue(expected in result)
+        
+        # missing assumedLong
+        input_dict = {'op': 'correct', 'lat': "asdsad", 'long': "adsa",
+                      'assumedLat': 'unknown', 'assumed1Long': '2016-01-17',
+                      'altitude': '03:15:99'}
+        result = Correct.validate_parameter(input_dict)
+        self.assertTrue(expected in result)
